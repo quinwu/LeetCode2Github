34ec8d06a20cdad70a22c3175f02327bc4a3894c
quinwu <wukuan1995@outlook.com>
2018-05-02 15:09:48 +0800

    update code

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
+import sys
 from src.leetcode import Leetcode
 from utils import get_config_from_file,CONFIG_FILE
 
-
-def run(quizs):
-    CONFIG = get_config_from_file(CONFIG_FILE)
-    leetcode = Leetcode(CONFIG)
+def run(leetcode):
+    quizs = [int(i) for i in sys.argv[1:]]
+    print ('download problems id is: {list}'.format(list=quizs))
     leetcode.run(quizs)
 
 if __name__ == '__main__':
-    quizs = [771]
-    run(quizs)
+    CONFIG = get_config_from_file(CONFIG_FILE)
+    leetcode = Leetcode(CONFIG)
+    run(leetcode)
